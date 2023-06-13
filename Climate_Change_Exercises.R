80431ed473f9c6b52189a7e4794f8c8dc8e44e13
cdelaguardia <cdelaguardia1987@gmail.com>
2023-06-13 13:44:00 -0600

    Adding temperature anomally analysis

---
+++ Climate_Change_Exercises.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(dslabs)
+data(temp_carbon)
+data(greenhouse_gases)
+data(historic_co2)
+
+
+# How many times larger were carbon emissions in the last year relative to the
+#  first year?
+
+# last year 
+last_year_emission <- temp_carbon |> filter(!is.na(carbon_emissions)) |>
+  pull(year) |> max()
+index_last_year <- which(temp_carbon$year == last_year_emission)
+
+# first year
+first_year_emission <- temp_carbon |> filter(!is.na(carbon_emissions)) |>
+  pull(year) |> min()
+index_first_year <- which(temp_carbon$year == first_year_emission)
+
+# ratio
+ratio <- temp_carbon$carbon_emissions[index_last_year]/temp_carbon$carbon_emissions[index_first_year]
+
+
+# How many degrees Celsius has temperature increased over the date range?
+# Compare the temperatures in the most recent year versus the oldest year.
+
+# Last year
+last_year_temp <- temp_carbon |> filter(!is.na(temp_anomaly)) |>
+  pull(year) |> max()
+index_last_year_temp <-  which(temp_carbon$year == last_year_temp) 
+
+print(paste("The temp anomaly in",temp_carbon$year[index_last_year_temp], 
+            "was of", temp_carbon$temp_anomaly[index_last_year_temp]))
+
+# first year
+first_year_temp <- temp_carbon |> filter(!is.na(temp_anomaly)) |>
+  pull(year) |> min()
+index_first_year_temp <-  which(temp_carbon$year == first_year_temp)
+
+print(paste("The temp anomaly in",temp_carbon$year[index_first_year_temp], 
+            "was of", temp_carbon$temp_anomaly[index_first_year_temp]))
